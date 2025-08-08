ecef8c2b1ae004fa29398b2767012ee3b0451f93
NicklasTSV <nicklas.janzen95@gmail.com>
2025-08-08 12:43:31 +0200

    config.js erstellen

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDglUE9tNiXgaoaMHQq2dMZCtk7-0DeCk4",
+  authDomain: "backup-c474e.firebaseapp.com",
+  projectId: "backup-c474e",
+  storageBucket: "backup-c474e.firebasestorage.app",
+  messagingSenderId: "149392616839",
+  appId: "1:149392616839:web:e7a7e14fca4d9827eff722"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
