07103e7886dd34d2a86f591849f883a5f4e08696
NicklasTSV <nicklas.janzen95@gmail.com>
2025-08-08 12:48:34 +0200

    config.js aktualisieren

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDglUE9tNiXgaoaMHQq2dMZCtk7-0DeCk4",
   authDomain: "backup-c474e.firebaseapp.com",
-  databaseURL: "https://backup-c474e-default-rtdb.europe-west1.firebasedatabase.app/"
+  databaseURL: "https://backup-c474e-default-rtdb.europe-west1.firebasedatabase.app/",
   projectId: "backup-c474e",
   storageBucket: "backup-c474e.firebasestorage.app",
   messagingSenderId: "149392616839",
